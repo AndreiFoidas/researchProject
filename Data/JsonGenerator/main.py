d0e9144c83fe8878bbb86b3e7fd825f59454f538
AndreiFoidas <62398994+AndreiFoidas@users.noreply.github.com>
2021-11-20 17:32:48 +0200

    Generated JSON files
    Generated JSON files with 10000 inputs for testing elasticsearch and opensearch and began the testing phase

--- Data/JsonGenerator/main.py
+++ Data/JsonGenerator/main.py
@@ -5,16 +5,19 @@ import urllib.request
 
 word_url = "https://www.mit.edu/~ecprice/wordlist.10000"
 
+
 class JSON:
     def __init__(self):
-        self.fileName = "books.json"
-        self.length = 10000
-        self.index = "books"
-        self.mainName = "author"
-        self.wordList = "theme"
-        self.nameList = "characters"
-        self.intName = "year"
-        self.mainWord = "title"
+        self.fileNameOpenSearch = "personsOS.json"
+        self.fileNameElectricSearch = "personsES.json"
+        self.length = 100000
+        self.index = "persons"
+        self.type = "person"
+        self.mainName = "name"
+        self.wordList = "hobbies"
+        self.nameList = "friends"
+        self.intName = "birthYear"
+        self.mainWord = "likes"
 
         response = urllib.request.urlopen(word_url)
         long_txt = response.read().decode()
@@ -22,21 +25,38 @@ class JSON:
 
 
     def createJSON(self):
-        file = open(self.fileName, "a")
+        fileOS = open(self.fileNameOpenSearch, "a")
+        fileES = open(self.fileNameElectricSearch, "a")
 
         for i in range(self.length):
+            print(i)
             random_genre1 = self.words[random.randint(0, len(self.words))-1]
             random_genre2 = self.words[random.randint(0, len(self.words))-1]
             random_tile = self.words[random.randint(0, len(self.words))-1] + " " + self.words[random.randint(0, len(self.words))-1]
-            random_year = random.randint(1700, 2022)
+            random_year = random.randint(1900, 2022)
 
-            file.write('{ "index" : { "_index": "' + self.index + '", "_id" : "' + str(i) + '" } }\n')
+            fileOS.write('{ "index" : { "_index": "' + self.index + '", "_id" : "' + str(i) + '" } }\n')
+            fileES.write('{ "index" : { "_index": "' + self.index + '", "_type": "' + self.type + '", "_id" : "' + str(i) + '" } }\n')
 
-            file.write('{"' + self.mainName + '": "' + names.get_full_name() + '", ')
-            file.write('"' + self.wordList + '": ["' + random_genre1 + '", "' + random_genre2 + '"], ')
-            file.write('"' + self.intName + '": ' + str(random_year) + ', ')
-            file.write('"' + self.nameList + '": ["' + names.get_full_name() + '", "' + names.get_full_name() + '", "' + names.get_full_name() + '"], ')
-            file.write('"' + self.mainWord + '": "' + random_tile + '"}\n')
+            stringMainName = '{"' + self.mainName + '": "' + names.get_full_name() + '", '
+            fileOS.write(stringMainName)
+            fileES.write(stringMainName)
+
+            stringWordList = '"' + self.wordList + '": ["' + random_genre1 + '", "' + random_genre2 + '"], '
+            fileOS.write(stringWordList)
+            fileES.write(stringWordList)
+
+            stringIntName = '"' + self.intName + '": ' + str(random_year) + ', '
+            fileOS.write(stringIntName)
+            fileES.write(stringIntName)
+
+            stringNameList = '"' + self.nameList + '": ["' + names.get_full_name() + '", "' + names.get_full_name() + '", "' + names.get_full_name() + '"], '
+            fileOS.write(stringNameList)
+            fileES.write(stringNameList)
+
+            stringMainWord = '"' + self.mainWord + '": "' + random_tile + '"}\n'
+            fileOS.write(stringMainWord)
+            fileES.write(stringMainWord)
 
 
 if __name__ == '__main__':
